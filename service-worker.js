7829ac292d8cdc502a7520e00476739fefcb2532
PCY-AUV110 <pengchiyou@icloud.com>
2026-08-05 14:38:54 +0800

    style: logo 侧边栏 70px / 登录页 100px

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "offerflow-v3-logo-size";
+const CACHE_NAME = "offerflow-v4-logo-70";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./supabase-config.js", "./manifest.json", "./logo.svg"];
 
 self.addEventListener("install", event => {
